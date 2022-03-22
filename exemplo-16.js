72ac90f666ffb006662b0b102d80e09a7610a045
Breno Gomes <brennogomez9@gmail.com>
2022-03-21 21:31:16 -0300

    feat: using functions: even or odd number example

---
+++ exemplo-16.js
@@ -0,0 +1,14 @@
+// Praticando funções
+
+//1. exemplo do número par ou ímpar:
+
+function parOuImpar(numero) {
+    if(numero % 2 == 0) {
+        return "Par!"
+    } else {
+        return "Ímpar!"
+    }
+}
+
+console.log(parOuImpar(6))
+
